b9bf23cefe2d12211097ff7eabdcc38a0d85ed34
laurasrf <laura.r.fernandes@viannasempre.com.br>
2021-05-27 17:55:18 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OlaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo!");
+            Console.WriteLine("Tecle Enter para sair");
+            Console.ReadKey();
+        }
+    }
+}
